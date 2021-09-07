36011b6887827cd46c09978e9a6cbb2033c050ef
Alan Vinícius Sega <alanvsega@gmail.com>
2021-09-07 06:20:45 -0300

    feat: implement logs with morgan

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import morgan from 'morgan'
+import path  from 'path'
+import { createStream } from 'rotating-file-stream'
 
 import { Authorization } from './middlewares/Authorization'
 import { ZipcodeRoutes } from './routes/ZipcodeRoutes'
@@ -7,7 +10,14 @@ const app = express()
 
 app.use(express.json())
 
+var logStream = createStream('zipcode-search.log', {
+  interval: '1d',
+  path: path.join(__dirname, '..', 'logs')
+})
+
+app.use(morgan('combined', { stream: logStream }))
+
 app.use(Authorization)
-app.use('/api/v1', ZipcodeRoutes)
+app.use('/api', ZipcodeRoutes)
 
 export { app }
