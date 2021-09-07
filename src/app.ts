d0ccf86d358667f7e3fa7765552661585e090030
Alan Vinícius Sega <alanvsega@gmail.com>
2021-09-07 07:14:48 -0300

    docs: API documented with OpenApi

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
+import yaml from 'yamljs'
 import path  from 'path'
+import swaggerUi from 'swagger-ui-express'
 import { createStream } from 'rotating-file-stream'
 
 import { Authorization } from './middlewares/Authorization'
 import { ZipcodeRoutes } from './routes/ZipcodeRoutes'
 
 const app = express()
+const swaggerDocs = yaml.load(path.join(__dirname, '../api.schema.yaml'))
 
 app.use(express.json())
 
@@ -16,6 +19,7 @@ var logStream = createStream('zipcode-search.log', {
 })
 
 app.use(morgan('combined', { stream: logStream }))
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 app.use(Authorization)
 app.use('/api', ZipcodeRoutes)
